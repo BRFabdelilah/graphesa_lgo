93654f37892135214d2e9db71402391fd4e7dbac
bouarafa abdelilah <152975419+BRFabdelilah@users.noreply.github.com>
2024-04-04 11:34:52 -0700

    Update cyle grand au petit.py

--- cyle grand au petit.py
+++ cyle grand au petit.py
@@ -7,16 +7,21 @@ G = nx.DiGraph()
 # Ajout des sommets au graphe
 G.add_nodes_from(range(1, 10))  # Nous avons 9 sommets
 
-# Ajout des arêtes au graphe
-edges = [(1, 2), (1, 6), (2, 3), (4, 2), (2, 5),(5, 6),
-         (3, 4), (3, 5), (4, 8), (4, 9), (6, 7), (7, 1)] 
+edges = [(1, 2), (1, 6), (2, 3), (4, 2), (2, 5),(5, 6),(10, 11),(11, 10),
+         (3, 4), (3, 5), ( 8,4), (4, 9),(9,8), (6, 7), (7, 1)]  # J'ai corrigé cette arête pour faire un cycle complet
 G.add_edges_from(edges)
 
-# Détection du cycle dans le graphe
-cycles = list(nx.simple_cycles(G))
+# Détection des cycles uniques dans le graphe
+cycles = nx.simple_cycles(G)
+
+# Filtrage des cycles uniques
+unique_cycles = []
+for cycle in cycles:
+    if all(node not in unique_cycle for unique_cycle in unique_cycles for node in cycle):
+        unique_cycles.append(cycle)
 
 # Trier les cycles en fonction de leur longueur (nombre de sommets)
-cycles.sort(key=lambda x: len(x), reverse=True)
+unique_cycles.sort(key=lambda x: len(x), reverse=True)
 
 # Affichage du graphe
 pos = nx.circular_layout(G)
@@ -26,7 +31,7 @@ nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=1000, arrows
 cycle_colors = ['red', 'green', 'blue', 'purple', 'orange', 'cyan', 'magenta', 'yellow', 'pink']
 
 # Affichage de chaque cycle avec une couleur différente
-for i, cycle_nodes in enumerate(cycles):
+for i, cycle_nodes in enumerate(unique_cycles):
     print(cycle_nodes)
     cycle_edges = [(cycle_nodes[i], cycle_nodes[i + 1]) for i in range(len(cycle_nodes) - 1)]
     cycle_edges.append((cycle_nodes[-1], cycle_nodes[0]))
